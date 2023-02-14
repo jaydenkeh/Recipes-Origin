1f9e5fd46c025224b50c5fc59504b802d4cc4859
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-14 11:32:39 +0800

    updated search bar with CSS

--- src/Components/Recipe/RecipeSearch/RecipeSearchMain.jsx
+++ src/Components/Recipe/RecipeSearch/RecipeSearchMain.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function RecipeSearchMain({ setSearch }) {
   return (
-    <div>
+    <div className="search-bar-container">
       <form
         onSubmit={(e) => {
           e.preventDefault();
           setSearch(e.target[0].value);
         }}
       >
-        <input type="text"></input>
-        <button type="submit">Search</button>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search by ingredient or cuisine"
+        ></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
     </div>
   );
